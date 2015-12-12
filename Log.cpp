cfa15186a93cca955f29ff59cd7db4af79b91c3f
Major Péter <majorpeter29@gmail.com>
2015-12-12 13:55:11 +0100

    buffered stdio logging

--- Log.cpp
+++ Log.cpp
@@ -2,11 +2,21 @@
 #include <Log/BufferedLog.h>
 #include "Log.h"
 
+#if LOG_MODE == LOG_MODE_STDIO
+#include "Protocol/StdioSerialInterface.h"
+#endif
+
+#if LOG_MODE == LOG_MODE_SERIAL
+#include "Protocol/VcpSerialInterface.h"
+#endif
+
+#include "BufferedLog.h"
+
 Log* Log::getInstance() {
     static Log* instance = NULL;
     if (instance == NULL) {
-#if LOG_MODE == LOG_MODE_ITM
-        instance = new StdioLog();
+#if LOG_MODE == LOG_MODE_STDIO
+        instance = new BufferedLog(new StdioSerialInterface());
 #else
   #if LOG_MODE == LOG_MODE_SERIAL
         instance = new BufferedLog(VcpSerialInterface::getInstance());
